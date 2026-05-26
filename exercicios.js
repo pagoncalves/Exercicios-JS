8d6ccdfeac88d115ca46dfcc09dcad21c08fe2fc
pagoncalves <patriciagpereira89@gmail.com>
2026-05-26 20:37:03 -0300

    exercicio 1

---
+++ exercicios.js
@@ -0,0 +1,17 @@
+//EXERCÍCIO 1 
+// Entrada: dois números
+// Saída: soma deles
+// Exemplo: soma(5, 3) → 8
+/*
+function soma(a,b){
+
+        return a + b;
+}
+ 
+console.log(soma(5,5))*/
+function soma(a,b){
+
+        return a + b;
+} 
+console.log("A soma é igual a: " + soma(8,5))
+
